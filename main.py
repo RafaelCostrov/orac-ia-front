b3515909677eafdf07bd582e1b1064bd8d5dd9c9
Rafael Costrov <rafael.costrov@gmail.com>
2025-09-04 12:11:55 -0300

    Frontend básico com streamlit

---
+++ main.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import requests
+from dotenv import load_dotenv
+import os
+
+
+BACKEND_URL = "http://localhost:5000/orac-ia"
+
+
+load_dotenv()
+SENHA_ORAC_IA = os.getenv('SENHA_ORAC_IA')
+
+st.set_page_config(page_title="Orac IA",
+                   page_icon="assets/orac.png", layout="wide")
+
+col_image, col_title = st.columns(
+    [1, 12], gap=None, vertical_alignment='center')
+with col_image:
+    st.image("assets/orac.png", width=100)
+with col_title:
+    st.title("Orac IA")
+
+col1, col2, col3 = st.columns([2, 2, 1])
+
+with col1:
+    nome_usuario = st.text_input("Seu nome")
+with col2:
+    email = st.text_input("Seu e-mail")
+with col3:
+    arquivo = st.file_uploader("Anexar documento", type=["pdf"])
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).markdown(msg["content"])
+
+if prompt := st.chat_input("Digite sua mensagem..."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").markdown(prompt)
+
+    data = {
+        "nome_usuario": nome_usuario,
+        "email": email,
+        "mensagem": prompt,
+        "senha": SENHA_ORAC_IA
+    }
+    files = {"arquivo": (arquivo.name, arquivo,
+                         arquivo.type)} if arquivo else None
+
+    try:
+        response = requests.post(BACKEND_URL, data=data, files=files)
+        if response.status_code == 200:
+            resposta = response.json().get("resposta", {})
+            output = resposta.get(
+                "output", "⚠️ Nenhuma resposta recebida.")
+        else:
+            output = f"⚠️ Erro {response.status_code}: {response.json().get('erro', response.text)}"
+    except Exception as e:
+        output = f"❌ Erro de conexão: {e}"
+
+    st.session_state.messages.append(
+        {"role": "assistant", "content": output})
+    st.chat_message("assistant").markdown(output, unsafe_allow_html=True)
